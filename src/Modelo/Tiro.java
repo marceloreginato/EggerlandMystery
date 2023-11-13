41383c0ec79cde1cc4c837a595cfc96140fc2a31
Rhayna Casado <rhaynacasado@usp.br>
2023-11-13 01:12:02 -0300

    ajuste fino

--- src/Modelo/Tiro.java
+++ src/Modelo/Tiro.java
@@ -7,36 +7,36 @@ import java.io.Serializable;
 
 public class Tiro extends Personagem implements Serializable {
 
-    private char c;
+    private char direcao;
  
-    public Tiro(String sNomeImagePNG, char c) {
+    public Tiro(String sNomeImagePNG, char direcao) {
         super(sNomeImagePNG, 't');
         this.bMortal = true;    
         this.bTiro = true;    
-        this.c = c;
+        this.direcao = direcao;
     }   
 
     public void autoDesenho() {
         super.autoDesenho();
-        if(this.c == 'r'){
+        if(this.direcao == 'r'){
             if(!moveRight())
                 Desenho.acessoATelaDoJogo().removePersonagem(this);
             if(!Desenho.acessoATelaDoJogo().ehPosicaoValida(this.getPosicao(), c, 't'))
                 Desenho.acessoATelaDoJogo().removePersonagem((this));
         }
-        if(this.c == 'l'){
+        if(this.direcao == 'l'){
             if(!moveLeft())
                 Desenho.acessoATelaDoJogo().removePersonagem(this);
             if(!Desenho.acessoATelaDoJogo().ehPosicaoValida(this.getPosicao(), c, 't'))
                 Desenho.acessoATelaDoJogo().removePersonagem((this));
         }
-        if(this.c == 'u'){
+        if(this.direcao == 'u'){
             if(!moveUp())
                 Desenho.acessoATelaDoJogo().removePersonagem(this);
             if(!Desenho.acessoATelaDoJogo().ehPosicaoValida(this.getPosicao(), c, 't'))
                 Desenho.acessoATelaDoJogo().removePersonagem((this));
         }
-        if(this.c == 'd'){
+        if(this.direcao == 'd'){
             if(!moveDown())
                 Desenho.acessoATelaDoJogo().removePersonagem(this);
             if(!Desenho.acessoATelaDoJogo().ehPosicaoValida(this.getPosicao(), c, 't'))
