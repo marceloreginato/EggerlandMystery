68a05f6901d16233a83193bf635ab84adcfe7da6
Rhayna Casado <rhaynacasado@usp.br>
2023-11-15 12:50:13 -0300

    ajuste fino

--- src/Modelo/Inimigo/InimigoAtirador.java
+++ src/Modelo/Inimigo/InimigoAtirador.java
@@ -1,5 +1,7 @@
 package Modelo.Inimigo;
 
+import Auxiliar.Posicao;
+
 public abstract class InimigoAtirador extends Inimigo{
     
     public InimigoAtirador(String sNomeImagePNG) {
@@ -10,6 +12,12 @@ public abstract class InimigoAtirador extends Inimigo{
     public abstract void atiraLeft();
     public abstract void atiraUp();
     public abstract void atiraDown();
+
+    @Override
+    /*Metodo que verifica se a posicao que objeto esta se movendo eh possivel.*/
+    public boolean ehPosicaoValida(Posicao p) {
+        return false;       /*Personagem nao pode se mover.*/
+    }
 }           
 
 
